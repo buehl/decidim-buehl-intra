a0247a37d378d9c6175c9ce981c9946a61192c0f
Eduard Itrich <eduard@itrich.net>
2019-09-27 09:12:05 +0200

    Moved config.i18n.load_path to environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,9 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  # Load locales in nested dictionaries under config/locales
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
